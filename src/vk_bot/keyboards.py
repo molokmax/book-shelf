2366b045d054a9852c24d3e864a4750f377855e3
Maksim Molokanov <maks.molokanov@gmail.com>
2026-05-18 22:33:18 +0300

    Update main keyboard

--- src/vk_bot/keyboards.py
+++ src/vk_bot/keyboards.py
@@ -3,10 +3,12 @@ from vk_api.keyboard import VkKeyboard
 def main_keyboard() -> VkKeyboard:
     """Create the keyboard for the /start command."""
     kb = VkKeyboard()
-    kb.add_button('/add')
     kb.add_button('/list')
     kb.add_button('/edit')
+    kb.add_button('/add')
+    kb.add_line()
     kb.add_button('/stats')
+    kb.add_button('/export')
     kb.add_button('/help')
     return kb
 
